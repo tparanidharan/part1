3d78638fdcf822d5839c151714c12569ae122d97
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-06-10 23:42:34 +0530

    Anecdote completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
 
+const MostVoted = ({copy,votescnt}) =>{
+  console.log("in most voted",copy,votescnt,Math.max( ...votescnt));
+const maxVotedAnnectode=copy[votescnt.indexOf(Math.max( ...votescnt))] || copy[0];
+return <>{maxVotedAnnectode} has been voted {Math.max( ...votescnt)} times</>
+}
+
+const Button = ({handleClick, text}) =>  <div><button onClick={handleClick}>{text}</button></div>
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -10,12 +17,25 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
   ]
-
+  const [votes,setvotes] = useState(new Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0)
-
+  const selAnecdote = () => {
+  const value = Math.floor((Math.random() * anecdotes.length)  );
+  setSelected(value);
+}
+const addVotes=() => {
+  const copy = [...votes];
+  copy[selected]+=1;
+  console.log("copy:",copy);
+  setvotes(copy);
+}
   return (
     <div>
       {anecdotes[selected]}
+      <Button handleClick={selAnecdote} text="next Anecdote"/>
+      <Button handleClick={addVotes} text="vote"/>
+      <h2> Most voted Anecdote</h2>
+      <MostVoted copy={anecdotes} votescnt={votes} />
     </div>
   )
 }
