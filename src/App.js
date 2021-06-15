1afe7e3432cb10c40b379d9e9d5399d230dac012
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-06-15 19:27:39 +0530

    Notes initial setup

--- src/App.js
+++ src/App.js
@@ -1,65 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const App = () => {
-  const course = {
-   name: 'Half Stack application development',
-   parts: [
-     {
-       name: 'Fundamentals of React',
-       exercises: 10
-     },
-     {
-       name: 'Using props to pass data',
-       exercises: 7
-     },
-     {
-       name: 'State of a component',
-       exercises: 14
-     }
-   ]
- }
+  const [notes, setNotes] = useState([])
 
-  return (
-    <div>
-    <Header course={course.name} />
-    <Content parts={course.parts}/>
-    <Total exercises={course.parts} />
-    </div>
-  );
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+  return <> <p> {notes[0]} </p></>
 }
 
-const Header= (props) => {
-  return (
-    <>
-      <h1> {props.course}  </h1>
-    </>
-  );
-}
-
-const Content =(parts) => {
- //console.log(parts.parts[0].name)
-  return(
-      <>
-        <Part part_name={parts.parts[0].name} excercises={parts.parts[0].exercises} />
-        <Part part_name={parts.parts[1].name} excercises={parts.parts[1].exercises} />
-        <Part part_name={parts.parts[2].name} excercises={parts.parts[2].exercises} />
-      </>
-    );
-}
-
-const Total = (props) =>{
-  return (<>
-    <p>Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}</p>
-  </>);
-}
-
-const Part =(props) => {
-  return (
-    <>
-    <p>
-       {props.part_name} {props.excercises}
-    </p>
-    </>
-  )
-}
-export default App;
+export default App
